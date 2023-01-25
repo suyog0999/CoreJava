994692cecb941e8dd74994b6336df8bf86a5f69b
technOrbit <suyograhane1883@gmail.com>
2023-01-25 20:20:21 +0530

    added

--- CoreJava/src/com/example/java/day3.java
+++ CoreJava/src/com/example/java/day3.java
@@ -3,10 +3,78 @@ package com.example.java;
 public class day3 {
 
 	public static void main(String[] args) {
+		// hard coded means without constructor
+		Person suyog=new Person();
+		System.out.println(suyog.firstName);
+		System.out.println(suyog.lastName);
+		suyog.displayName();
 		
 		
-		// TODO Auto-generated method stub
+		// with constructor
+		PersonA mayur = new PersonA("mayur","rahane");
+		mayur.displayName();
+	
+		PersonA mrunmayi = new PersonA("mrunmayi","deshmukh");
+		mrunmayi.displayName();
+		
+		
+		// set/get method
+		
+		PersonB amit = new PersonB();
+		amit.setFirstName("amit");
+		amit.setLastName("nehe");
+		amit.displayName();
+		
 
+	}
+
+}
+
+// hard coded means without constructor
+
+class Person {
+
+String firstName = "Suyog";
+String lastName = "Rahane";
+
+public void displayName() {
+	System.out.println(this.firstName + this.lastName);
+}
+}
+
+// with constructor
+
+class PersonA {
+String firstName;
+String lastName ;
+
+public PersonA(String fn , String ln) {
+	this.firstName = fn;
+	this.lastName = ln;				
+}
+public void displayName() {
+	System.out.println(this.firstName + this.lastName);
+}
+	
+}
+
+// set/get method
+
+class PersonB {
+
+	String firstName;
+	String lastName;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void displayName() {
+		System.out.println(this.firstName + this.lastName);
 	}
 
 }
